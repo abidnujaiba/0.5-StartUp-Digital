6cab57b5b741dcc8eedf49d4ab730a26c52d3cb8
ABID <abid.nujaiba@mail.ugm.ac.id>
2023-05-29 15:40:19 +0700

    add section 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+"use client";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import HowItWorks from "@/components/HowItWorks";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="w-full h-screen bg-krem grid place-content-center">
+    <section className="w-full min-h-screen bg-krem grid place-content-center">
       <Navbar />
       <Hero />
+      <HowItWorks />
     </section>
   );
 }
