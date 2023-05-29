e6984ca3d8249b3ac3715ffec94999875c1b46bb
Andyan Yoga <65155073+zenxiety@users.noreply.github.com>
2023-05-29 13:33:42 +0700

    add - hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen bg-orange-400 grid place-content-center">
+    <section className="w-full h-screen bg-krem grid place-content-center">
       <Navbar />
-      <h1 className="font-krifon">hebitut</h1>
-    </main>
+      <Hero />
+    </section>
   );
 }
